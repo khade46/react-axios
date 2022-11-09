0d05b3b1f9b8660f5813986a34b24d0762f84d02
Aakash Kumar <khadeaakashb704@gmail.com>
2022-11-09 14:16:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import Get from './get';
+import Post from './post';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+      <Post/>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
